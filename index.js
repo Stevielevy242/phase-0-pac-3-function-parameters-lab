536d6576d5ee71e173b89e2e98baf926477d3120
Steven Levy <slevy399@yahoo.com>
2023-02-09 17:08:00 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(firstName = "User") {
+    return(`Hi, my name is ${firstName}.`);
+  }
+
+  function introductionWithLanguage(name, language) {
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    if (language === "JavaScript")
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    else
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
